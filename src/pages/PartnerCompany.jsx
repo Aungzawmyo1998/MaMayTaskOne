13fcab718e003ac599c4c6127fee6aa9119284c9
Aungzawmyo1998 <mraungzaw303@gmail.com>
2024-11-21 12:06:43 +0630

    first commit

--- src/pages/PartnerCompany.jsx
+++ src/pages/PartnerCompany.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass, faXmark, faScrewdriverWrench, faChevronDown, faLocationDot, faBuilding } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faXmark, faScrewdriverWrench, faChevronDown, faLocationDot, faBuilding, faHandshake, faBars, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faHourglassHalf } from '@fortawesome/free-regular-svg-icons'
 
 // css
 import '../styles/partnerCompany.css'
 
 function PartnerCompany() {
+
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (tab_number) => {
+        setToggleState(tab_number)
+    }
+
     return (
         <div id='partnerCompany'>
-            <div className="data-container"></div>
+            <div className="data-container">
+                <div className="data-header">
+                    <div className='right-side'>
+                        <div className="group">
+                            全12,345件
+                        </div>
+                        <div>
+                            <select className='select-type'>
+                                <option value="">全て</option>
+                                <option value="">募集中</option>
+                                <option value="">募集終了</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="tabs-container">
+                        <div onClick={()=> {toggleTab(1)}} className={ toggleState === 1 ? "active-tab tab" : "tab" } >全て</div>
+                        <div onClick={()=> {toggleTab(2)}} className={ toggleState === 2 ? "active-tab tab" : "tab" }> <FontAwesomeIcon className='icon' icon={faHandshake} /> 協力会社</div>
+                        <div onClick={()=> {toggleTab(3)}} className={ toggleState === 3 ? "active-tab tab" : "tab" }><FontAwesomeIcon className='icon' icon={faHourglassHalf} /> 許可待ち</div>
+                        <div onClick={()=> {toggleTab(4)}} className={ toggleState === 4 ? "active-tab tab" : "tab" }> 
+                            <div className="confirmation-icon">
+                                <FontAwesomeIcon className='icon' icon={faBars} />
+                                <FontAwesomeIcon className='bars-icon' icon={faCheck} />
+                            </div> 
+                            申請確認
+                        </div>
+                    </div>
+                </div>
+                <div className="content-container">
+                    <div className={ toggleState === 1 ? "active-content all-content" : " all-content"}>Content One</div>
+                    <div className={ toggleState === 2 ? "active-content partner-content" : " partner-content"}>Content two</div>
+                    <div className={ toggleState === 3 ? "active-content permission-content" : " permission-content"}>Content three</div>
+                    <div className={ toggleState === 4 ? "active-content confirmation-content" : "confirmation-content"}>Content four</div>
+                </div>
+            </div>
             <div className="search-container">
                 <div className="search-bar">
                     <div className="search-icon">
